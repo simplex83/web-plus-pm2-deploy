cf9d85ee3344fbe8366c1858cce98a456d82e808
Oleg <simplex83@gmail.com>
2023-08-31 12:32:19 +0500

    deploy setting

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'backend',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp -Cr .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js --env production`,
+    },
+  },
+};
